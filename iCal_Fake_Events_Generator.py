7f1a67f9fab04071e5004cb04d60315b25c91dc4
zylai <40312846+zylai@users.noreply.github.com>
2023-01-03 22:57:14 +0000

    Update iCal_Fake_Events_Generator.py

--- iCal_Fake_Events_Generator.py
+++ iCal_Fake_Events_Generator.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import os.path
 from os import path
 import sys
-from datetime import datetime
+from datetime import datetime,timedelta
 import uuid
 import random
 import re
@@ -23,76 +23,69 @@ if path.exists(file_location):
 	print("\033[91m" + file_location + " already exists. Aborting..." + "\033[0m")
 	sys.exit(1)
 
-regex_year = "^[12][0-9]{3}$"
-regex_month = "^(0?[1-9]|1[012])$"
-regex_time = "([01]\d|2[0-3])[0-5][0-9]"
+while True:
+	user_input_year = input("Enter the year you'd like to create junk events for: ")
+	if bool(re.search("^[12][0-9]{3}$", user_input_year)):
+		break
+	print("\033[91mPlease enter a valid year between 1000 to 2999\033[0m")
 
-do {
-	user_input_year = Read-Host -Prompt "Enter the year you'd like to create junk events for"
-	if (user_input_year -notmatch regex_year) {
-		Write-Host "Please enter a valid year between 1000 to 2999" -ForegroundColor Red
-	}
-} until (user_input_year -match regex_year)
+while True:
+	user_input_month = input("Enter the month you'd like to create junk events for: ")
+	if bool(re.search("(^[1-9]$)|(^1[0-2]$)", user_input_month)):	# Disallow leading zero in month as `datetime` does not allow it
+		break
+	print("\033[91mPlease enter a valid month between 1 to 12\033[0m")
 
-do {
-	user_input_month = Read-Host -Prompt "Enter the month you'd like to create junk events for"
-	if (user_input_month -notmatch regex_month) {
-		Write-Host "Please enter a valid month between 1 to 12" -ForegroundColor Red
-	}
-} until (user_input_month -match regex_month)
+regex_time = "^(([0-9])|([0-1][0-9])|([2][0-3]))([0-5][0-9])$"
 
-do {
-	do {
-		user_input_begin_time = Read-Host -Prompt "Enter your start time in 24-hour format without colons (ex: 0830)"
-		if (user_input_begin_time -notmatch regex_time) {
-			Write-Host "Please enter a valid 24-hour time without colons" -ForegroundColor Red
-		}
-	} until (user_input_begin_time -match regex_time)
+while True:
+	while True:
+		user_input_begin_time = input("Enter your start time in 24-hour format without colons (ex: 0830): ")
+		if bool(re.search(regex_time, user_input_begin_time)):
+			break
+		print("\033[91mPlease enter a valid 24-hour time without colons\033[0m")
 
-	do {
-		user_input_end_time = Read-Host -Prompt "Enter your end time in 24-hour format without colons (ex: 1700)"
-		if (user_input_end_time -notmatch regex_time) {
-			Write-Host "Please enter a valid 24-hour time without colons" -ForegroundColor Red
-		}
-	} until (user_input_end_time -match regex_time)
+	while True:
+		user_input_end_time = input("Enter your end time in 24-hour format without colons (ex: 1700): ")
+		if bool(re.search(regex_time, user_input_end_time)):
+			break
+		print("\033[91mPlease enter a valid 24-hour time without colons\033[0m")
 
-	if (user_input_begin_time -gt user_input_end_time) {
-		Write-Host "Start time is later than end time. Please try again" -ForegroundColor Red
-	}
-} until (user_input_begin_time -lt user_input_end_time)
+	if user_input_begin_time < user_input_end_time:
+		break
+	print("\033[91mStart time is later than end time. Please try again\033[0m")
 
-$DayBeginHour = "user_input_begin_time".toString().Substring(0,2)
-$DayBeginMinute = "user_input_begin_time".toString().Substring(2,2)
-$DayEndHour = "user_input_end_time".toString().Substring(0,2)
-$DayEndMinute = "user_input_end_time".toString().Substring(2,2)
+day_begin_hour = user_input_begin_time[0:2]
+day_begin_minute = user_input_begin_time[2:4]
+day_end_hour = user_input_end_time[0:2]
+day_end_minute = user_input_end_time[2:4]
 
-$DaysInMonth = [datetime]::DaysInMonth(user_input_year,user_input_month)
+days_in_month = monthrange(user_input_year, user_input_month)[1]
 
 calendar_template_begin = """BEGIN:VCALENDAR
 PRODID:-//ZYLAI//iCal Fake Events Generator//EN
 VERSION:2.0
 METHOD:PUBLISH"""
 
-echo calendar_template_begin >> file_location
+ics_file = open(file_location, "a")
+ics_file.write(calendar_template_begin)
 
-for ($Day = 1; $Day -le $DaysInMonth; $Day++) {
-	$Date = Get-Date -Year user_input_year -Month user_input_month -Day $Day
+for day in range(1, days_in_month + 1):
+	date = datetime(user_input_year, user_input_month, day)
 
-	if ($Date.DayOfWeek -eq "Saturday" -Or $Date.DayOfWeek -eq "Sunday") {
+	if ((date.strftime('%A') == "Saturday") or (date.strftime('%A') == "Sunday")):
 		continue
-	}
 
-	$DayBegin = Get-Date -Year user_input_year -Month user_input_month -Day $Day -Hour $DayBeginHour -Minute $DayBeginMinute
-	$DayEnd = Get-Date -Year user_input_year -Month user_input_month -Day $Day -Hour $DayEndHour -Minute $DayEndMinute
+	day_begin = datetime(user_input_year, user_input_month, day, day_begin_hour, day_begin_minute)
+	day_end = datetime(user_input_year, user_input_month, day, day_end_hour, day_end_minute)
 
-	$EventEnd = $DayBegin
+	event_end = day_begin
 
-	while ($EventEnd -lt $DayEnd) {
-		$BreakLength = New-TimeSpan -Minutes random.choice(break_length_options)
-		$EventEnd = $EventEnd + $BreakLength
-		$EventBegin = $EventEnd
-		$EventLength = New-TimeSpan -Minutes random.choice(event_length_options)
-		$EventEnd = $EventBegin + $EventLength
+	while (event_end < day_end):
+		break_length = timedelta(minutes = random.choice(break_length_options))
+		event_end = event_end + break_length
+		event_begin = event_end
+		event_length = timedelta(minutes = random.choice(event_length_options))
+		event_end = event_begin + event_length
 
 		event_template = """BEGIN:VEVENT
 CLASS:PRIVATE
@@ -103,13 +96,13 @@ DTEND:$($EventEnd.ToUniversalTime().ToString("yyyyMMddTHHmm00Z"))
 PRIORITY:9
 SEQUENCE:0
 TRANSP:OPAQUE
-UID:str(uuid.uuid4())
-END:VEVENT"""
+UID:{}
+END:VEVENT""".format(, , , str(uuid.uuid4()))
 
-		echo event_template >> file_location
-	}
-}
+		ics_file.write(event_template)
 
 calendar_template_end = "END:VCALENDAR"
 
-echo calendar_template_end >> file_location
+ics_file.write(calendar_template_end)
+
+ics_file.close
